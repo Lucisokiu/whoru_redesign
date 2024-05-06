6672fb4da006670140075e690eb1dc41cb19d799
Lucisokiu <nguyenminhnhut435@gmail.com>
2024-05-06 15:09:56 +0700

    feat(feed): Pagination

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # **WHORU**
 
-A social networking platform, serves as a place to connect people with the features listed below. I draw inspiration from popular social networks such as Facebook, Instagram, Twitter, etc. This is an incomplete version and will undergo many upgrades in the future.
+A social networking platform, serves as a place to connect people with the features listed below. I draw inspiration from popular social networks such as Facebook, Instagram, Twitter, etc. This is an incomplete version and will undergo many upgrades in the future.  
 Follow me to know newest update
 
 # **FEATURE**  
@@ -8,7 +8,7 @@ Login/Register
 Verify code  
 Create Feed  
 Like/Comment/Share  
-Apply Filter to Camera
+Apply Filter to Camera  
 View list Like/Comment/Share  
 Search Users  
 User Location  
--- lib/src/pages/feed/widget/skeleton_loading.dart
+++ lib/src/pages/feed/widget/skeleton_loading.dart
@@ -4,30 +4,32 @@ import 'package:shimmer/shimmer.dart';
 class MySkeletonLoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 1, // Số lượng item bạn muốn hiển thị trong danh sách
-      itemBuilder: (context, index) {
-        return Shimmer.fromColors(
-          baseColor: Colors.grey[300]!,
-          highlightColor: Colors.grey[100]!,
-          child: ListTile(
-            leading: CircleAvatar(
-              backgroundColor: Colors.white,
-              radius: 30,
+    return Container(
+      child: ListView.builder(
+        itemCount: 1,
+        itemBuilder: (context, index) {
+          return Shimmer.fromColors(
+            baseColor: Colors.grey[300]!,
+            highlightColor: Colors.grey[100]!,
+            child: ListTile(
+              leading: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 30,
+              ),
+              title: Container(
+                height: 16,
+                width: double.infinity,
+                color: Colors.white,
+              ),
+              subtitle: Container(
+                height: 12,
+                width: double.infinity,
+                color: Colors.white,
+              ),
             ),
-            title: Container(
-              height: 16,
-              width: double.infinity,
-              color: Colors.white,
-            ),
-            subtitle: Container(
-              height: 12,
-              width: double.infinity,
-              color: Colors.white,
-            ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
